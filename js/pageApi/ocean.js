1c2b60bdaf176280b1c123bf10e05ceb0c0f2038
nftoctopus <73518827+nftoctopus@users.noreply.github.com>
2021-08-05 10:00:14 +0800

    Update ocean.js

--- js/pageApi/ocean.js
+++ js/pageApi/ocean.js
@@ -7,10 +7,10 @@ let isDeposit = JSON.parse(window.localStorage.getItem('isDeposit'))
 // let recipient = 'ATurAq5tYF7XakZfCgRBTWND6tQoKVznARE'
 // let BuyRecipient = 'AUBMBq1Ej1DvXBghTENXWe1S52Suqf8qrCe'
 
-let contractId = ‘CCBtpBGWMSUHXSGBzruV7qxDVmRh8RMaXr9’
-let tokenId = ‘TWZzuRJST8uhsdc484eHoyDSRKyxGqJMn7NiNpmhC’
-let recipient = ‘ARR2BNk7sNUzXz2c2P6Syz9vysYqcuGCxRJ’
-let BuyRecipient = ‘ARR2BNk7sNUzXz2c2P6Syz9vysYqcuGCxRJ
+let contractId = 'CCBtpBGWMSUHXSGBzruV7qxDVmRh8RMaXr9';
+let tokenId = 'TWZzuRJST8uhsdc484eHoyDSRKyxGqJMn7NiNpmhC';
+let recipient = 'ARR2BNk7sNUzXz2c2P6Syz9vysYqcuGCxRJ';
+let BuyRecipient = 'ARR2BNk7sNUzXz2c2P6Syz9vysYqcuGCxRJ';
 
 //质押
 async function depositToken() {
